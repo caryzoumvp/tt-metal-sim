4c91601948a4e3445cf4ea388f6800367de9b69c
cary <cary@123.com>
2026-01-22 10:29:15 +0800

    supress TT_THROW for DEBUG

--- tt_env.sh
+++ tt_env.sh
@@ -13,4 +13,5 @@ export TT_METAL_WATCHER=1
 export TT_METAL_DPRINT_CORES='(0,0),(0,1)'
 export TT_METAL_RISCV_DEBUG_INFO=1
 export TT_METAL_WATCHER_CORES='(0,0),(0,1)' 
-export TT_METAL_DPRINT_ONE_FILE_PER_RISC=1
+#export TT_METAL_DPRINT_ONE_FILE_PER_RISC=0
+export TT_METAL_DPRINT_FILE=$HOME/tensix/tt-metal/generated/dprint/dprint.log
