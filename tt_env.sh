b80ab028c7765c7fea766cc98085ad995d1ec198
cary.zou <cary.zou1988@gmail.com>
2026-02-04 01:04:49 +0000

    bump up to upstream 9231910

--- tt_env.sh
+++ tt_env.sh
@@ -1,12 +1,9 @@
-export PATH=$HOME/sfpi/build/sfpi/compiler/bin/:$PATH
-export TT_UMD_SIMULATOR=$HOME/tensix/tt-metal/tt_metal/third_party/umd/sim_dev/libttsim.so
-export TT_METAL_SIMULATOR=$HOME/tensix/tt-metal/tt_metal/third_party/umd/sim_dev/libttsim.so
+export TT_UMD_SIMULATOR=/workspaces/tensix/tt-metal/tt_metal/third_party/umd/sim_dev/libttsim.so
+export TT_METAL_SIMULATOR=/workspaces/tensix/tt-metal/tt_metal/third_party/umd/sim_dev/libttsim.so
 export TT_METAL_SLOW_DISPATCH_MODE=1
-export TT_LOGGER_LEVEL=debug
+export TT_LOGGER_LEVEL=info
 export TT_METAL_LOG_KERNELS_COMPILE_COMMANDS=1
 export TT_METAL_KERNEL_MAP=1
-export PYTHONPATH=$HOME/.local/lib/python3.10/site-packages/
-export SYSTEMC_HOME=$HOME/systemc-2.3.1/
 
 export TT_METAL_RISCV_DEBUG_INFO=1
 export TT_METAL_WATCHER=1
