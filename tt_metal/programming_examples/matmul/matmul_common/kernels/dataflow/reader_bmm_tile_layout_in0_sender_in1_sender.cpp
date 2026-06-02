07975075c8b31d14cfd556702ec7a8e8d7826515
cary.zou <cary.zou1988@gmail.com>
2026-06-02 03:29:03 +0000

    add profiling marker for matmul_multicore_reuse and matmul_multicore_reuse_mcast

--- tt_metal/programming_examples/matmul/matmul_common/kernels/dataflow/reader_bmm_tile_layout_in0_sender_in1_sender.cpp
+++ tt_metal/programming_examples/matmul/matmul_common/kernels/dataflow/reader_bmm_tile_layout_in0_sender_in1_sender.cpp
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include "api/dataflow/dataflow_api.h"
 #include "hostdevcommon/common_values.hpp"
+#include "../profile_log.h"
 
 void kernel_main() {
     // in0 tensor args
@@ -101,6 +102,7 @@ void kernel_main() {
         uint32_t in0_tensor_current_block_start_tile_id = in0_tensor_start_tile_id;
         uint32_t in1_tensor_current_block_start_tile_id = in1_tensor_start_tile_id;
         for (uint32_t block = 0; block < num_blocks; block++) {
+            profile_mark(PHASE_READER_BLOCK_START, block);
             cb_reserve_back(cb_id_in0, in0_block_num_tiles);
             l1_write_addr_in0 = get_write_ptr(cb_id_in0);
 
@@ -120,14 +122,17 @@ void kernel_main() {
                 in0_tensor_row_start_tile_id += in0_tensor_stride_h;
             }
             in0_tensor_current_block_start_tile_id += in0_tensor_next_block_stride;
+            profile_mark(PHASE_READER_IN0_AFTER_READS_ISSUED, block);
 
             // Barrier! make sure the reads are done
             noc_async_read_barrier();
+            profile_mark(PHASE_READER_IN0_AFTER_BARRIER, block);
 
             // wait until all in0 mcast destinations have atomically incremented the in0 semaphore_addr (i.e. its value
             // should be in0_mcast_num_dests), then reset the semaphore_addr value back to zero for the next block
             noc_semaphore_wait(in0_mcast_sender_semaphore_addr_ptr, in0_mcast_num_dests);
             noc_semaphore_set(in0_mcast_sender_semaphore_addr_ptr, 0);
+            profile_mark(PHASE_READER_IN0_WAIT_RECEIVERS, block);
 
             // Now we have the block in the CB address, we can mcast to dests!
             uint64_t in0_multicast_data_addr = get_noc_multicast_addr(
@@ -159,8 +164,10 @@ void kernel_main() {
             // num_dests must not include source, since we are NOT really doing a local copy!
             noc_semaphore_set_multicast(
                 in0_mcast_receiver_semaphore_addr, in0_mcast_receiver_semaphore_noc_addr, in0_mcast_num_dests);
+            profile_mark(PHASE_READER_IN0_AFTER_MCAST, block);
 
             cb_push_back(cb_id_in0, in0_block_num_tiles);
+            profile_mark(PHASE_READER_IN0_AFTER_PUSH, block);
 
             // Operand 1
             cb_reserve_back(cb_id_in1, in1_block_num_tiles);
@@ -182,14 +189,17 @@ void kernel_main() {
                 in1_tensor_row_start_tile_id += in1_tensor_stride_h;
             }
             in1_tensor_current_block_start_tile_id += in1_tensor_next_block_stride;
+            profile_mark(PHASE_READER_IN1_AFTER_READS_ISSUED, block);
 
             // Barrier! make sure the reads are done
             noc_async_read_barrier();
+            profile_mark(PHASE_READER_IN1_AFTER_BARRIER, block);
 
             // wait until all in1 mcast destinations have atomically incremented the in1 semaphore_addr (i.e. its value
             // should be in0_mcast_num_dests), then reset the semaphore_addr value back to zero for the next block
             noc_semaphore_wait(in1_mcast_sender_semaphore_addr_ptr, in1_mcast_num_dests);
             noc_semaphore_set(in1_mcast_sender_semaphore_addr_ptr, 0);
+            profile_mark(PHASE_READER_IN1_WAIT_RECEIVERS, block);
 
             // Now we have the block in the CB address, we can mcast to dests!
             uint64_t in1_multicast_data_addr = get_noc_multicast_addr(
@@ -221,8 +231,10 @@ void kernel_main() {
             // num_dests must not include source, since we are NOT really doing a local copy!
             noc_semaphore_set_multicast(
                 in1_mcast_receiver_semaphore_addr, in1_mcast_receiver_semaphore_noc_addr, in1_mcast_num_dests);
+            profile_mark(PHASE_READER_IN1_AFTER_MCAST, block);
 
             cb_push_back(cb_id_in1, in1_block_num_tiles);
+            profile_mark(PHASE_READER_IN1_AFTER_PUSH, block);
         }
         if (bcast_B == 0) {
             in1_tensor_start_tile_id += KtNt;
